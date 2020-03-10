b63448f1a86f6ad3665e9b8c7c678fc8d6be2401
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-10 13:17:40 +0100

    Add first version

--- src/checker.sh
+++ src/checker.sh
@@ -4,7 +4,7 @@ checker::check() {
   local -r patterns="$1"
   local -r message="$2"
 
-  local -r modified_files=$(github_actions::get_commit_modified_files "$GITHUB_SHA")
+  local -r modified_files=$(github::get_commit_modified_files "$GITHUB_SHA")
 
   local -r critial_modified_files=$(echo "$patterns" | coll::map_2 str::contains "$modified_files")
 
