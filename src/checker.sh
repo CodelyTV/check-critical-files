b3e95ee2991b32d3baca94a697a7a7bce477fbf4
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-10 12:29:38 +0100

    Add first version

--- src/checker.sh
+++ src/checker.sh
@@ -10,8 +10,8 @@ checker::check() {
 #  log::message "$patterns"
 #  log::message "$message"
 
-  log::message "LA BUENA 4444"
-  echo "$patterns" | coll::map_2 str::contains "$patterns"
+  log::message "LA BUENA 55555"
+  echo "$patterns" | coll::map_2 str::contains "$modified_files"
 
   log::message "Total modifications: $modified_files"
 }
