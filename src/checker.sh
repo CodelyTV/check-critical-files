ea1673961cdbe91d9df3d9b5669e353d0f4b8812
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-23 13:31:35 +0100

    critical commit multiple files

--- src/checker.sh
+++ src/checker.sh
@@ -7,12 +7,10 @@ checker::check() {
   local -r commit_sha=$(github_actions::commit_sha)
   local -r modified_files=$(github::get_commit_modified_files "$commit_sha")
 
-  local -r critical_modified_files=$(echo "$patterns" | coll::map_2 str::contains "$modified_files")
-
-  log::message "Files: $critical_modified_files"
+  local -r critical_modified_files=$(echo "$patterns" | coll::map_2 str::contains "$modified_files" | sed ':a;N;$!ba;s/\n/\r\n/g')
 
   if [ -n "$critical_modified_files" ]; then
-    local -r final_message="$message - $critical_modified_files"
+    local -r final_message="$message\r\nModified files:\r\n$critical_modified_files"
 
     log::message "Message: $final_message"
     github::comment_commit "$commit_sha" "$final_message"
